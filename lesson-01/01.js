e60e8d13d497a5d2951192e4d3aa44b8f6b0811e
YuryR <myaccount136@gmail.com>
2024-11-09 07:23:57 +0200

    homeWork_01.1_js, first commit

---
+++ lesson-01/01.js
@@ -0,0 +1,14 @@
+// С помощью цикла while найдите сумму чисел от 1 до 20
+// Результат присвойте переменной sum
+
+// your code
+
+let sum = 0
+
+let count = 0
+while (count < 20) {
+    count++
+    sum += count
+}
+
+console.log(sum);
