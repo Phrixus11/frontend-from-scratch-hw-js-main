4221a76ae5c86e0ab62da0f1ea120958f97aad7b
YuryR <myaccount136@gmail.com>
2024-11-21 00:16:53 +0200

    homeWork_03_js

--- lesson-03/02.js
+++ lesson-03/02.js
@@ -1,4 +1,9 @@
 // Разработайте функцию isEven, которая определяет, чётное ли число.
 // Функция должна возвращать true, если число чётное, и false — в противном случае
 
-function isEven() {}
+function isEven(x) {
+    return x % 2 ? false : true ;
+}
+
+console.log(isEven(2));
+console.log(isEven(4));
--- lesson-03/03.js
+++ lesson-03/03.js
@@ -1,3 +1,14 @@
 // Разработайте функцию findLargest, которая принимает три числа и возвращает наибольшее из них.
 
-function findLargest() {}
+function findLargest(a, b, c) {
+    if (a > b && a > c) {
+        return a
+    } else if (b > a && b > c) {
+        return b
+    }
+    return c
+}
+
+console.log(findLargest(1, 2, -3));
+console.log(findLargest(5, 2, 3));
+console.log(findLargest(5, 2, 9));
