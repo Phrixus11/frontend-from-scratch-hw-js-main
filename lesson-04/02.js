87e9f81c71b28e5ba879b4c0fa36b03b3e24b0c9
YuryR <myaccount136@gmail.com>
2024-11-27 23:05:05 +0200

    homeWork_04.2_js

--- lesson-04/02.js
+++ lesson-04/02.js
@@ -15,17 +15,44 @@
 Подсказка: можно использовать функцию `includesElement`, которую мы написали ранее. Переписывать её не нужно, она доступна по всему проекту за счёт hoisting.
 */
 
-
+//версия 1
 function findUniqueElements(array) {
-    let newArray = [];
+    let newArray = [array[0]];
+    let isIssue = false;
+    console.log(newArray);
+    
     for (let i = 0; i < array.length; i++) {
-        if (includesElement(newArray, array[i])) {
-        } else {
+        for (let i2 = 0; i2 < newArray.length; i2++) {
+            if (array[i] === newArray[i2]) {
+                isIssue = false;
+                break
+            } else {
+                isIssue = true;
+            }
+            
+        }
+        if (isIssue) {
             newArray.push(array[i])
         }
+        
     }
     return newArray
 }
+
+console.log(findUniqueElements([1, 2, 3, 7, 7, 10, 2, 1, 4, 10]));
+
+
+// //версия 2
+// function findUniqueElements(array) {
+//     let newArray = [];
+//     for (let i = 0; i < array.length; i++) {
+//         if (includesElement(newArray, array[i])) {
+//         } else {
+//             newArray.push(array[i])
+//         }
+//     }
+//     return newArray
+// }
 
 
 
--- lesson-04/03.js
+++ lesson-04/03.js
@@ -17,31 +17,53 @@ findCommonElements([1, 2, 3], [2, 3, 4]) // [2, 3]
 Подсказка: можно использовать функцию `includesElement`, которую мы написали ранее. Переписывать её не нужно, она доступна по всему проекту за счёт hoisting.
 */
 
-
-
-//Вариант 2
+//Вариант 1
 function findCommonElements(array1, array2) {
-    let newArray = [];
-    for (let i = 0; i < array2.length; i++) {
-        if (includesElement(array1, array2[i])) {
-            if (includesElement(newArray, array2[i])) {
-            } else {
-                newArray.push(array2[i])
+    let newArray = [array1[0]];
+    let isIssue = false;
+    for (let i1 = 0; i1 < array1.length; i1++) {
+        for (let i2 = 0; i2 < array2.length; i2++) {
+            if (array1[i1] === array2[i2]) {
+                for (let i3 = 0; i3 < newArray.length; i3++) {
+                    if (array2[i2] === newArray[i3]) {
+                        isIssue = false
+                        break
+                    } else {
+                        isIssue = true
+                    }
+                }
+                if (isIssue) {
+                    newArray.push(array1[i1])
+                }
             }
         }
     }
     return newArray
 }
 
-
-// console.log(findCommonElements(["banana", 2, 3, 8, 5, 3, 4, 5], [7, 8, "banana", 2, 3]));
-
-
-// function includesElement(array, element) {
-//     for (let i = 0; i < array.length; i++) {
-//         if (array[i] === element) {
-//             return true;
+// //Вариант 2
+// function findCommonElements(array1, array2) {
+//     let newArray = [];
+//     for (let i = 0; i < array2.length; i++) {
+//         if (includesElement(array1, array2[i])) {
+//             if (includesElement(newArray, array2[i])) {
+//             } else {
+//                 newArray.push(array2[i])
+//             }
 //         }
 //     }
-//     return false;
+//     return newArray
 // }
+
+
+console.log(findCommonElements([2, 5, 3, 4, 'banana', 1, 5, 2, 3, 4, 'banana'], [1, 2, 3, 4, 'banana', 1, 2, 3, 4, 'banana', 5]));
+
+
+function includesElement(array, element) {
+    for (let i = 0; i < array.length; i++) {
+        if (array[i] === element) {
+            return true;
+        }
+    }
+    return false;
+}
