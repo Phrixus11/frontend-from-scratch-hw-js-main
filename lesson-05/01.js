ec3b5420eecb23edb2637b31ba92879923be1279
YuryR <myaccount136@gmail.com>
2024-12-06 11:52:07 +0200

    homeWork_05.1_js_object

--- lesson-05/01.js
+++ lesson-05/01.js
@@ -15,5 +15,14 @@ const game = {
     gold: 250,
     lumber: 100,
   },
-  addResource() {}
+  addResource(resource, amout) {
+    if (resource in this.resources) {
+      this.resources[resource] += amout
+    } else {
+      console.log("Invalid resource");      
+    }
+  }
 }
+
+game.addResource("gold", 10)
+console.log(game.resources);
